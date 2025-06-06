883d3ee66720d20c0218b35d082313d0f5082a9f
bibikarine <160857488+bibikarine@users.noreply.github.com>
2025-06-06 07:18:21 -0300

    .

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'PERGUNTAS',
+    'Qual é o principal impacto ambiental causado pela expansão das cidades sobre o campo?',
+    'Desmatamento e perda de áreas agrícolas.'
+)
+
+criaCartao(
+    'PERGUNTAS',
+    ' Por que muitas pessoas se mudam do campo para a cidade?',
+    'Em busca de trabalho, estudo e melhores condições de vida.'
+)
+
+criaCartao(
+    'PERGUNTAS',
+    'Como se chama a área de transição entre o campo e a cidade?',
+    'Periferia ou zona periurbana.'
+)
+
+criaCartao(
+    'CURIOSIDADES',
+    'Você sabia que muitos alimentos típicos das festas urbanas vêm diretamente do campo?',
+    'Produtos como milho, amendoim e mandioca são colhidos no interior e viram delícias nas festas das cidades!'
+)
+
+criaCartao(
+    'CURIOSIDADES',
+    'O campo e a cidade se unem também na economia durante as festas?',
+    'Com certeza! Agricultores vendem mais produtos, e comerciantes da cidade aproveitam para impulsionar as vendas com itens típicos!'
+)
+
+criaCartao(
+    'CURIOSIDADES',
+    'Você sabia que as festas juninas nas cidades são inspiradas em tradições do campo?',
+    'As quadrilhas, roupas caipiras e até as danças vieram da vida rural e foram adaptadas para o ambiente urbano.'
+)
